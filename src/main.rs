18b77eb3d0c32df9a9b7aeb153d86a9f97f4c166
Fabrice Desré <fabrice@desre.org>
2023-11-27 11:03:29 -0800

    Try to add files to the root dir in spawned tasks

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,61 @@
+use anyhow::Result;
 use chrono::Utc;
 use futures::future::join_all;
-use rand::rngs::OsRng;
+use rand::{rngs::OsRng, Rng};
+use std::sync::Arc;
 use wnfs::{
     common::MemoryBlockStore,
     private::{
         forest::{hamt::HamtForest, traits::PrivateForest},
-        PrivateFile,
+        PrivateDirectory, PrivateFile,
     },
 };
 
-async fn create_file_from(mut forest: HamtForest, store: &MemoryBlockStore, content: Vec<u8>) {
-    // Create a new file (detached from any directory)
-    let _file = PrivateFile::with_content_rc(
+async fn create_file_in_dir(
+    mut dir: Arc<PrivateDirectory>,
+    mut forest: HamtForest,
+    store: MemoryBlockStore,
+    name: String,
+    content: Vec<u8>,
+) -> Result<()> {
+    let now = Utc::now();
+    let source = PrivateFile::with_content_rc(
         &forest.empty_name(),
-        Utc::now(),
+        now,
         content,
         &mut forest,
-        store,
+        &store,
         &mut OsRng,
     )
     .await
     .unwrap();
+
+    let dest = dir
+        .open_file_mut(&[name], true, Utc::now(), &mut forest, &store, &mut OsRng)
+        .await?;
+    dest.copy_content_from(&source, now);
+
+    Ok(())
 }
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
-    let store = &MemoryBlockStore::default();
+    let store = MemoryBlockStore::default();
     let rng = &mut OsRng;
     let forest = HamtForest::new_rsa_2048(rng);
+    let root = Arc::new(PrivateDirectory::new(&forest.empty_name(), Utc::now(), rng));
 
     let mut handles = vec![];
 
-    for _i in 0..100 {
-        let content = Vec::with_capacity(42 * 1024);
-        handles.push(tokio::spawn(create_file_from(
+    for i in 0..100 {
+        let content = format!("{{ \"index\": {}, \"random\": {} }}", i, rng.gen::<f64>());
+
+        handles.push(tokio::spawn(create_file_in_dir(
+            Arc::clone(&root),
             forest.clone(),
             store,
-            content,
+            format!("file_{}.json", i),
+            content.as_bytes().to_vec(),
         )))
     }
 
